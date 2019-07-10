b7dc49cf7d813a6d43cdf192e5230a29a71dd2c6
Ian Hwang <ihwang@hatchb.com>
2019-07-09 21:02:48 -0400

    initial commit

---
+++ src/com/Ian/trillio/entities/User.java
@@ -0,0 +1,10 @@
+package com.Ian.trillio.entities;
+
+public class User {
+    private long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+}
